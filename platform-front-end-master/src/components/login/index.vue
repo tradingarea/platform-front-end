41088646faf36e1c9c9993673bda4984b3cc63ea
xinxin1214 <1173015873@qq.com>
2019-08-30 16:59:48 +0800

    登录页面；

---
+++ platform-front-end-master/src/components/login/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id='login'>
+    <div class="outer_label">
+      <!--<img class="inner_label login_logo" src="../../assets/logo.png">-->
+    </div>
+    <div class="login_form">
+      <input type="text"  class="qxs-ic_user qxs-icon"  placeholder="用户名" v-model="userName">
+      <input type="text"  class="qxs-ic_password qxs-icon"  placeholder="密码" v-model="password">
+      <!--<button class="login_btn el-button el-button&#45;&#45;primary is-round" type="primary" round>登录</button>-->
+      <el-button class="login_btn" @click.native="login" type="primary" round :loading="isBtnLoading">登录</el-button>
+      <div style="margin-top: 10px">
+        <span style="color: #000099;" @click="login">司机账号登陆</span><span style="float: right;color: #A9A9AB">忘记密码？</span>
+      </div>
+    </div>
+  </div>
+</template>
+ 
+ 
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style>
+  .login_form {
+    padding-top: 10%;
+    padding-left: 10%;
+    padding-right: 10%;
+  }
+ 
+  .login_logo {
+    height: 100%;
+  }
+  .login_btn {
+    width: 100%;
+    font-size: 16px;
+    background: -webkit-linear-gradient(left, #000099, #2154FA); /* Safari 5.1 - 6.0 */
+    background: -o-linear-gradient(right, #000099, #2154FA); /* Opera 11.1 - 12.0 */
+    background: -moz-linear-gradient(right, #000099, #2154FA); /* Firefox 3.6 - 15 */
+    background: linear-gradient(to right, #000099 , #2154FA); /* 标准的语法 */
+    filter: brightness(1.4);
+  }
+</style>
